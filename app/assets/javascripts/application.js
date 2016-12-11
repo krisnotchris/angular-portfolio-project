66da50477f1ab7c9531365dbc433072be1e77704
krisnotchris <krishenderson@gmail.com>
2016-12-11 14:58:54 -0600

    changed stylesheet to sass, updated javascripts and stylesheets manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require angular-ui-router
+//= require angular-devise
+//= require angular-rails-templates
+//= require bootstrap-sprockets
 //= require_tree .
